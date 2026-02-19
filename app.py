d504a0e3955d67f3e36789dc48a5acf51c6039e1
Allan-O-H <allan.oh98@gmail.com>
2026-02-19 10:33:45 -0500

    Comments added for clairty

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
 import openpyxl as xl #This creates an alias for openpyxl
 from openpyxl.chart import BarChart, Reference
 
+# Loading the Excel workbook into a variable that behaves like a dictionary
+# where worksheet names are the key
 wb = xl.load_workbook('transactions.xlsx')
 sheet = wb['Sheet1']
+
+# Accessing cells by Name
 cell = sheet['a1']
+# Accessing cells by Coordinates 
 cell = sheet.cell(1,1)
 
+# This loop iterates every row stating from row 2, assuming row 1 is a header row,
+# 
 for row in range(2, sheet.max_row + 1):
     cell = sheet.cell(row,3)
     corrected_price = cell.value * 0.9
@@ -18,8 +25,10 @@ values = Reference(sheet,
           min_col=4,
           max_col=5)
 
+# Adding a Bar Chart into variable "chart", which was imported in -> "from openpyxl.chart import BarChart, Reference"
 chart = BarChart()
 chart.add_data(values)
 sheet.add_chart(chart, 'e2')
 
+# Saves the excel sheet into a new sheet
 wb.save('transactions2.xlsx')
